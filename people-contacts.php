faeaae8d953ee724e27aa4a2a1d6c91a408a7bae
EltonnJulio <eltonnjulio@gmail.com>
2022-08-18 12:56:16 +0200

    first commit

---
+++ people-contacts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     People Contacts
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     A Wordpress plugin to manage contacts
+ * Author:          Eltonn
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     people-contacts
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         People_Contacts
+ */
+
+// Your code starts here.
